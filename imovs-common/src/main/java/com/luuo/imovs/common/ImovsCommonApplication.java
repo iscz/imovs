4941834dd12f9e7611767cbd4e82619ca77a6018
iscz <im.guchen@gmail.com>
2020-07-21 00:09:52 +0800

    init

---
+++ imovs-common/src/main/java/com/luuo/imovs/common/ImovsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.luuo.imovs.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImovsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImovsCommonApplication.class, args);
+    }
+
+}
---
+++ imovs-common/src/main/java/com/luuo/imovs/common/config/MybatisPlusConfig.java
@@ -0,0 +1,32 @@
+package com.luuo.imovs.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.luuo.imovs.common.mapper"})
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 乐观锁mybatis插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+
+}
