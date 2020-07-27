63acce5b099f2e0309931783853b879d74934923
iscz <im.guchen@gmail.com>
2020-07-27 23:36:27 +0800

    eureka server

---
+++ imovs-registry/src/test/java/com/luuo/imovs/registry/ImovsRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luuo.imovs.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class ImovsRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
